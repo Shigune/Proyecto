c714f87e52aff1b7618024765df7eb944dc4fd52
Shigune <Joshua.badilla@gmail.com>
2023-10-22 13:27:04 -0600

    Correcion desempate

--- X/segundo avance.py
+++ X/segundo avance.py
@@ -3,4 +3,4 @@ import csv
 with open("C:\Copa_Mundial_2023.csv") as copa:
           reader = csv.reader(copa)
           for fila in reader:
-              print(f"Local: {fila[0]} Visita: {fila[1]} Goles local: {fila[2]} Goles visita: {fila[3]} Tarjetas casa: {fila[4]} Tarjetas visita: {fila[5]}")
+              print(f"Equipo Local: {fila[0]} / Equipo Visitante: {fila[1]} / Goles equipo local: {fila[2]} / Goles equipo visita: {fila[3]} / Tarjetas del equipo local: {fila[4]} / Tarjetas del equipo local: {fila[5]}")
